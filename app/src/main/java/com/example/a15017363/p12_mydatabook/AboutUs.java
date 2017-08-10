2aedd8ddcbd876be510ea49e542dfb6406a27079
15017363 <15017363@myrp.edu.sg>
2017-08-10 15:08:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15017363/p12_mydatabook/AboutUs.java
@@ -0,0 +1,34 @@
+package com.example.a15017363.p12_mydatabook;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class AboutUs extends AppCompatActivity {
+
+    ActionBar ab;
+    TextView tv,tvCreated;
+    ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        tv = (TextView)findViewById(R.id.textView);
+        tvCreated = (TextView)findViewById(R.id.textViewCreated);
+        iv = (ImageView)findViewById(R.id.imageView);
+
+        tvCreated.setText("Created By : Ah Chong, Peter, Mary");
+        String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/8/80/Republic_Polytechnic_Logo.jpg";
+        Picasso.with(this).load(imageUrl).into(iv);
+        tv.setText("C347 Android Programming II Republic Polytechnic");
+    }
+}
---
+++ app/src/main/java/com/example/a15017363/p12_mydatabook/AnniversaryFragment.java
@@ -0,0 +1,99 @@
+package com.example.a15017363.p12_mydatabook;
+
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AnniversaryFragment extends Fragment {
+
+    Button btnEdit;
+    TextView tvAnni;
+    DatabaseReference myRef;
+
+
+    public AnniversaryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.anniversaryfragment, container, false);
+
+        btnEdit = (Button)view.findViewById(R.id.btnFragAnni);
+        tvAnni = (TextView)view.findViewById(R.id.textViewAnni);
+
+        myRef = FirebaseDatabase.getInstance().getReference().child("Anniversary");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String val = dataSnapshot.getValue(String.class);
+                tvAnni.setText(val);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final View mView = inflater.inflate(R.layout.edit_bio, null);
+
+                builder.setTitle("Edit Anniversary")
+                        // Set text for the positive button and the corresponding
+                        //  OnClickListener when it is clicked
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                EditText etBio = (EditText)mView.findViewById(R.id.editTextBio);
+                                String a = etBio.getText().toString();
+                                myRef.setValue(a);
+                            }
+                        })
+                        // Set text for the negative button and the corresponding
+                        //  OnClickListener when it is clicked
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Toast.makeText(getContext(), "You clicked no", Toast.LENGTH_LONG).show();
+                            }
+                        });
+                // Create the AlertDialog object and return it
+                builder.setView(mView);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+            }
+        });
+
+        return view;
+    }
+
+}
